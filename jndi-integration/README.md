efadcc3921961d946057f132387ef392aa32af2a
Daniel Qian <chanjarster@gmail.com>
2016-08-20 15:20:20 +0800

    add jndi integration example

---
+++ jndi-integration/README.md
@@ -0,0 +1,7 @@
+运行 mvn verify -Pserver -DARTEMIS_HOME=${ARTEMIS_HOME_PATH} 创建artemis
+
+运行 ./target/server0/bin/artemis run 启动artemis
+
+运行 mvn exec:java 执行程序
+
+
---
+++ jndi-integration/src/main/java/me/chanjar/App.java
@@ -0,0 +1,75 @@
+package me.chanjar;
+
+import javax.jms.*;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+  private final AtomicInteger count = new AtomicInteger(100);
+
+  public static void main(String[] args) throws NamingException, JMSException {
+
+    InitialContext initialContext = new InitialContext();
+    ConnectionFactory connectionFactory = (ConnectionFactory) initialContext.lookup("ConnectionFactory");
+
+    Queue queue = (Queue) initialContext.lookup("exampleQueue");
+    App app = new App();
+
+    try (Connection connection = connectionFactory.createConnection()) {
+      app.send(connection, queue);
+      app.receive(connection, queue);
+    }
+
+  }
+
+  public void send(Connection connection, Queue requestQueue) throws JMSException {
+
+    try (Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE)) {
+
+      MessageProducer producer = session.createProducer(requestQueue);
+
+      int i = count.get();
+      while (i != 0) {
+        producer.send(session.createTextMessage("test"));
+        i--;
+      }
+
+    }
+
+  }
+
+  public void receive(Connection connection, Queue requestQueue) {
+
+    try {
+
+      connection.start();
+
+      try (Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE)) {
+
+        MessageConsumer consumer = session.createConsumer(requestQueue);
+
+        consumer.setMessageListener(message -> {
+          try {
+            count.decrementAndGet();
+            System.out.println(count + ":" + ((TextMessage) message).getText());
+          } catch (JMSException e) {
+            e.printStackTrace();
+          }
+        });
+
+        while (count.get() != 0) {
+
+        }
+      }
+
+    } catch (JMSException e) {
+      e.printStackTrace();
+    }
+
+  }
+}
